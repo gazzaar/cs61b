3ff68a59c0572de393a75c83628563cad4240c0c
gazzaar <gazzarDev@gmail.com>
2024-11-09 10:48:38 +0200

    Practice ch1 concepts

---
+++ code-examples/week-1/LargeDemo.java
@@ -0,0 +1,16 @@
+public class LargeDemo {
+  public static int Larger(int x, int y) {
+    if (x > y) {
+      return x;
+    } else {
+      return y;
+    }
+
+  }
+
+  public static void main(String[] args) {
+    System.out.print(Larger(4, 8));
+
+  }
+
+}
