bdde4e3ddd8070d781c5e6f16d275b1ab50dd1f2
gazzaar <gazzarDev@gmail.com>
2024-11-21 08:10:11 +0200

    Fix exercise 1.2.1

--- code-examples/week-1/Dog.java
+++ code-examples/week-1/Dog.java
@@ -17,4 +17,23 @@ public class Dog {
 
   }
 
+  public static Dog maxDog(Dog d1, Dog d2) {
+    if (d1.weightInPounds > d2.weightInPounds) {
+      System.out.println("Dog 1 s bigger");
+      return d1;
+    }
+
+    System.out.println("Dog 2 s bigger");
+    return d2;
+  }
+
+  public static void main(String[] args) {
+
+    Dog d1 = new Dog(20);
+    Dog d2 = new Dog(70);
+    d1.makeNoise();
+    System.out.println(Dog.maxDog(d1, d2));
+
+  }
+
 }
