d19e60d7878c321b264cfad7fd0e05296c49576f
gazzaar <gazzarDev@gmail.com>
2024-12-02 10:54:23 +0200

    Implement collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,17 +1,19 @@
+import java.util.List;
+
 /** Class that prints the Collatz sequence starting from a given number.
  *  @author YOUR NAME HERE
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
+            if (n == 1) {
+                return n;
+            } else if (n % 2 == 0) {
+                return n / 2;
+            } else {
+                return 3 * n + 1;
+            }
     }
 
     public static void main(String[] args) {
