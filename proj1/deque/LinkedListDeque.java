6f7bdaaa0eb3af2c5f5a93a635b6ea17779cb788
gazzaar <gazzarDev@gmail.com>
2025-01-25 17:33:10 +0200

    Make initial structure

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,4 +1,58 @@
 package deque;
-public  class LinkedListDeque{
+public  class LinkedListDeque<T>{
+    public class IntNode{
+       public T item;
+       public IntNode next;
+
+       public IntNode(T i, IntNode rest){
+            item = i;
+            next = rest;
+       }
+    }
+    private IntNode  sentinel;
+    private int size;
+    public LinkedListDeque(){
+        sentinel = new IntNode(null,null);
+        size = 0;
+    }
+
+    public void addFirst(T item){
+        // TODO: implement add first
+
+    }
+
+    public void addLast(T item){
+        // TODO: implement add last
+    }
+
+    public boolean isEmpty(){
+        // TODO: implement is empty
+        return true;
+    }
+
+    public int size(){
+        // TODO: implement size
+        return 0;
+    }
+
+    public void printDeque(){
+        // TODO: implement print deque
+
+    }
+
+    public T removeFirst(){
+        // TODO: implement remove first item
+            return null;
+    }
+
+    public T removeLast(){
+        // TODO: implement remove last item
+        return null;
+    }
+
+    public T get(int index){
+        // TODO: implement get item on index
+            return null;
+    }
 
 }
